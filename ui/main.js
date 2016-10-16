86abfb107f0c6375bc099b8383ebfd54c2b58ff6
Savan567 <14bit027@nirmauni.ac.in>
2016-10-16 11:23:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+
+var button=document.getelementbyId("counter");
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getelementbyId("count");
+    span.innerHTML=counter.toString();
+};
