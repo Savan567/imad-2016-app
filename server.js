38866632e3f760bae818f96686e5f26cf31f31af
Savan567 <14bit027@nirmauni.ac.in>
2016-10-16 10:40:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/:articleName', function (req, res) {
   var articleName=req.params.articleName;
   res.send(path.join(createTemplate(articles[articleName])));
 });
-app.get('/ui/index.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
